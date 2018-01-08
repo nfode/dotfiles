36a1159a92fc06fe870031a2d3cea50c5c2a031b
Nick Fode <nickfode@posteo.de>
2018-01-08 21:16:18 +0100

    Initial commit

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+DISPLAY1="$(xrandr -q | grep 'eDP1\|VGA-1' | cut -d ' ' -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar big &
+
+DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar big &
+
+echo "Bars launched..."
---
+++ .zprofile
@@ -0,0 +1,12 @@
+if [ -n "$DESKTOP_SESSION" ];then
+    eval $(gnome-keyring-daemon --start)
+    export SSH_AUTH_SOCK
+fi
+export QT_QPA_PLATFORMTHEME="qt5ct"
+PATH="$HOME/.local/bin:$PATH"
+export TERM=konsole
+export TERMINAL=$TERM
+if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+    exec startx &> /dev/null
+   exit
+fi
