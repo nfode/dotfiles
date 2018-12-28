e51737848c7d4db8735ebeb64be51e2b4b9a648a
Nick Fode <dev@nfode.de>
2018-12-29 00:07:15 +0100

    Add mouse button config for mx master

--- .zshconfig/fzf.zsh
+++ .zshconfig/fzf.zsh
@@ -1,9 +1,3 @@
-fd() {
-  local dir
-  dir=$(find ${1:-.} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
 
 prev="[[ \$(file --mime {}) =~ binary ]] && echo {} is a binary file || (highlight -O ansi -l {} || cat {}) 2> /dev/null | head -500"
 export FZF_DEFAULT_COMMAND='find *'
