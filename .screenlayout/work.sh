524376c730b2135421bdd915cdc9422e02139b55
Nick Fode <dev@nfode.de>
2021-01-27 15:31:44 +0100

    Update

--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --mode 3440x1440 --pos 0x0 --rotate normal --output HDMI2 --off --output HDMI3 --off --output VIRTUAL1 --off
+xrandr --output eDP1 --off --output DP1 --off --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --mode 3440x1440 --pos 0x0 --rotate normal --output HDMI3 --off --output VIRTUAL1 --off
